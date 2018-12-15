173942ed4c9b911b425f5b07d7d0ba7d900028f7
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-15 14:54:51 -0500

    " update"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
 var morgan = require('morgan');
+var path = required('path');
 
 var app = express();
 
@@ -9,8 +10,11 @@ var app = express();
 
 app.use(morgan('combined'));
 // app.use(morgan('tiny'));
+// app.get('/', (req, res) => {
+//     res.send('Hello from my library app');
+// });
 app.get('/', (req, res) => {
-    res.send('Hello from my library app');
+    res.sendFile(path.join(__dirname, 'views/index/html'));
 });
 
 app.listen(3000, () => {
